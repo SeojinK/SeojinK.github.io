d744cb7289f09bf4bdeb7a31c52eee18433b57f1
Seojin24 <jine9323@gmail.com>
2019-03-03 21:50:21 +0900

    first commit

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+##
+# This section should match your Makefile
+##
+#cp -r ../old_carpedm20.github.com/solar ./output/
+#cp -r ../old_carpedm20.github.com/ndrive ./output/
+#cp -r ../old_carpedm20.github.com/randomwall ./output/
+make publish; git add . --all; git commit -m "update"; git push origin master;
+echo "cp -r ./output/* ../carpedm20.github.io/"
+echo "cd ../carpedm20.github.io"
+echo "cp ./about/index.html ."
+echo "git add ."
+echo "git commit -m \"update\""
+echo "git push"
